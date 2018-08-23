b5d63745722d3699c2ba77b62d789b893a722219
Arash Tadjiki <atadjiki@yahoo.com>
2018-08-23 12:32:42 -0600

    LinkedList Based Snake

--- SnackAttack_CrossPlatform/SnackAttack/Snake.cs
+++ SnackAttack_CrossPlatform/SnackAttack/Snake.cs
@@ -13,33 +13,42 @@ namespace SnackAttack.Desktop
         int snakeLength; // amnount of snake nodes, including head
         int spacing; //track every nth head positions (0 will look really mushed)
         int collisionModifier = 10;
-        int wiggleModifier = 1;
-        int tailWiggleModifier = 10;
-        bool up = false, down = false, left = false, right = false;
 
-        Queue<Vector2> previousPositions;
-        List<Texture2D> snakeBody;
-        List<Vector2> positions;
+        bool up = false, down = false, left = false, right = false;
+        enum direction { up, down, left, right};
+
+        //Queue<Vector2> previousPositions;
+        //List<Texture2D> snakeBody;
+        List<SnakeNode> snakeBody;
+        //List<Vector2> positions;
         BoundingBox headBox = new BoundingBox();
+
+        public class SnakeNode{
+
+            public Vector2 velocity = new Vector2();
+            public Vector2 position = new Vector2();
+            public Vector2 oldPosition = new Vector2();
+            public Texture2D texture = null; 
+            };
 
         public Snake(float initialX, float initialY)
         {
             snakeSpeed = 100f;
-            snakeLength = 16;
-            spacing = 15;
+            snakeLength = 50;
+            spacing = 35;
 
-            snakeBody = new List<Texture2D>(snakeLength);
-            positions = new List<Vector2>(snakeLength);
-            previousPositions = new Queue<Vector2>(snakeLength * spacing);
+            //snakeBody = new List<Texture2D>(snakeLength);
+            snakeBody = new List<SnakeNode>(snakeLength);
 
-
-            //add and increment offsets in loop
+           // positions = new List<Vector2>(snakeLength);
+            //previousPositions = new Queue<Vector2>(snakeLength * spacing);
 
             //initialize all node positions 
             for (int i = 0; i < snakeLength; i++)
             {
-                positions.Add(new Vector2(initialX, initialY));
-
+                //positions.Add(new Vector2(initialX, initialY));
+                snakeBody.Add(new SnakeNode());
+                snakeBody[i].position = new Vector2(initialX, initialY);
             }
 
         }
@@ -47,70 +56,39 @@ namespace SnackAttack.Desktop
         public void loadSnake(Texture2D head, Texture2D body, Texture2D tail)
         {
             //add snake head
-
-            snakeBody.Add(head);
+           // snakeBody.Add(head);
+            snakeBody[0].texture = head;
 
             //add snake body
             for (int i = 0; i < snakeLength; i++)
             {
-                snakeBody.Add(body);
+                // snakeBody.Add(body);
+                snakeBody[i].texture = body;
             }
 
             //add snake tail
-            snakeBody.Add(tail);
+            //snakeBody.Add(tail);
+            snakeBody[snakeLength - 1].texture = tail;
         }
 
         public void UpdateSnakePositions(KeyboardState kstate, GameTime gameTime, GraphicsDeviceManager graphics, BoundingBox obstacleBox)
         {
 
             UpdateBoundingBox();
-            var head = positions[0];
+            // var head = positions[0];
+            var head = snakeBody[0].position;
             var lastPreviousPosition = head;
 
             //store previous head position in queue
-            if (previousPositions.Count == snakeLength * spacing)
-            {
-                previousPositions.Dequeue();
-            }
-            //add offsets here
-            if (up || down)
-            {
-                lastPreviousPosition.X += (float)new Random().Next(-wiggleModifier, wiggleModifier);
-            }
-            else if (left || right)
-            {
-                lastPreviousPosition.Y += (float)new Random().Next(-wiggleModifier, wiggleModifier);
-            }
-            previousPositions.Enqueue(lastPreviousPosition);
+            // if (previousPositions.Count == snakeLength * spacing)
+            // {
+            //     previousPositions.Dequeue();
+            // }
 
-            //tweak tail nodes
-            int tailLength = previousPositions.Count / 5;
-            if (tailLength >= 1)
-            {
-                //get last tailLength nodes
-                List<Vector2> tail = new List<Vector2>();
-                if (previousPositions.Count > tailLength)
-                {
-                    while (tailLength > 0)
-                    {
-
-                        var tailNode = previousPositions.ToArray()[previousPositions.Count - tailLength];
-                        if (up || down)
-                        {
-                            tailNode.X += (float)new Random().Next(-tailWiggleModifier, tailWiggleModifier);
-                        }
-                        else if (left || right)
-                        {
-                            tailNode.Y += (float)new Random().Next(-tailWiggleModifier, tailWiggleModifier);
-                        }
-
-                        tailLength--;
-                    }
-                }
-            }
+            // previousPositions.Enqueue(lastPreviousPosition);
+            snakeBody[0].oldPosition = snakeBody[0].position;
 
             //move head first
-
             if (kstate.IsKeyDown(Keys.Up))
             {
                 head.Y -= snakeSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
@@ -136,8 +114,8 @@ namespace SnackAttack.Desktop
                 up = false; down = false; left = false; right = true;
             }
 
-            head.X = Math.Min(Math.Max(snakeBody[0].Width / 2, head.X), graphics.PreferredBackBufferWidth - snakeBody[0].Width / 2);
-            head.Y = Math.Min(Math.Max(snakeBody[0].Height / 2, head.Y), graphics.PreferredBackBufferHeight - snakeBody[0].Height / 2);
+            head.X = Math.Min(Math.Max(snakeBody[0].texture.Width / 2, head.X), graphics.PreferredBackBufferWidth - snakeBody[0].texture.Width / 2);
+            head.Y = Math.Min(Math.Max(snakeBody[0].texture.Height / 2, head.Y), graphics.PreferredBackBufferHeight - snakeBody[0].texture.Height / 2);
 
             //check collision, if this move is allowed, store the position - if not, stay where we are!
             if (headBox.Intersects(obstacleBox))
@@ -163,26 +141,31 @@ namespace SnackAttack.Desktop
             }
 
 
-            positions[0] = head;
+            // positions[0] = head;
+            snakeBody[0].position = head;
 
             // loop through list, set positions for all nodes while incrementing positions
             for (int i = 1; i < snakeLength; i++)
             {
 
                 //set position
-                Vector2 temp = positions[i];
+                //  Vector2 temp = positions[i];
+                Vector2 temp = snakeBody[i].position;
 
-                if (previousPositions.Count != 0 && previousPositions.ToArray().Length > i * spacing)
+                //if (previousPositions.Count != 0 && previousPositions.ToArray().Length > i * spacing)
+                //{
+                //    temp = previousPositions.ToArray()[i * spacing];
+                //}
+                if(snakeBody[i-1].oldPosition != null)
                 {
-                    temp = previousPositions.ToArray()[i * spacing];
+                    snakeBody[i].position = snakeBody[i - 1].oldPosition;
                 }
-
 
                 //do NOT offset snake here!
 
 
                 //store back in list
-                positions[i] = temp;
+                //positions[i] = temp;
 
             }
         }
@@ -193,19 +176,31 @@ namespace SnackAttack.Desktop
             for (int i = 0; i < snakeLength; i++)
             {
 
+                // spriteBatch.
+                //Draw(snakeBody[i], positions[i], null, Color.White, 0f, new Vector2(snakeBody[i].Width / 2, snakeBody[0].Height / 2), Vector2.One, SpriteEffects.None, 0f);
                 spriteBatch.
-                Draw(snakeBody[i], positions[i], null, Color.White, 0f, new Vector2(snakeBody[i].Width / 2, snakeBody[0].Height / 2), Vector2.One, SpriteEffects.None, 0f);
+                    Draw(snakeBody[i].texture, snakeBody[i].position, null, Color.White, 0f, new Vector2(snakeBody[i].texture.Width / 2, snakeBody[0].texture.Height / 2), Vector2.One, SpriteEffects.None, 0f);
             }
         }
 
         //keeps track of snake head bounding box
         protected void UpdateBoundingBox()
         {
-            this.headBox.Min.X = positions[0].X;
-            this.headBox.Min.Y = positions[0].Y;
-            this.headBox.Max.X = positions[0].X + snakeBody[0].Width;
-            this.headBox.Max.Y = positions[0].Y + snakeBody[0].Height;
+            //this.headBox.Min.X = positions[0].X;
+            //this.headBox.Min.Y = positions[0].Y;
+            //this.headBox.Max.X = positions[0].X + snakeBody[0].Width;
+            //this.headBox.Max.Y = positions[0].Y + snakeBody[0].Height;
+
+            this.headBox.Min.X = snakeBody[0].position.X;
+            this.headBox.Min.Y = snakeBody[0].position.Y;
+            this.headBox.Max.X = snakeBody[0].position.X + snakeBody[0].texture.Width;
+            this.headBox.Max.Y = snakeBody[0].position.Y + snakeBody[0].texture.Height;
+
         }
+
+        protected void SolveSegment(Vector2 a, Vector2 b)
+        { }
+            
     }
 
 
