c18b3c556f1554aaf2bf7d751f56347aec7aea95
Mitnileshbhai Doshi <u1212719@ad.utah.edu>
2018-08-26 21:20:20 -0600

    First Instance of mouse AI

--- SnackAttack_CrossPlatform/SnackAttack/Mice.cs
+++ SnackAttack_CrossPlatform/SnackAttack/Mice.cs
@@ -26,13 +26,14 @@ namespace SnackAttack.Desktop
         private float yOffset = 50f;
         private Vector2 gotoLocation;
         private int index = 2;
+        private int previousIndex;
 
 
         public Mice(float initialX, float initialY)
         {
             backWidth = initialX;
             backHeight = initialY;
-            miceSpeed = 120f;
+            miceSpeed = 210f;
             totalMicePoints = 4;
             
             micePointLocations = new List<Vector2>(totalMicePoints);
@@ -44,6 +45,7 @@ namespace SnackAttack.Desktop
 
             miceLocation = micePointLocations[0];
             gotoLocation = micePointLocations[1];
+            previousIndex = 1;
         }
 
 
@@ -58,19 +60,74 @@ namespace SnackAttack.Desktop
             UpdateBoundingBox();
             float x_direction;
             float y_direction;
-
+            int xPercent;
+            int yPercent;
             //getting the direction of mice
             
-
+            previousIndex = index;
             if (miceLocation.X == gotoLocation.X && miceLocation.Y == gotoLocation.Y)
             {
-                index = index % 4;
+                
+                
+                xPercent = (int)((headPosition.X * 100) / backWidth);
+                yPercent = (int)((headPosition.Y * 100) / backHeight);
+                int difference = xPercent - yPercent;
+                Console.WriteLine("xPercent: " + xPercent + "YPercent: " + yPercent + " difference: " + difference + " Previous Index: " + previousIndex);
+                if (xPercent >= 50) {
+                    if (yPercent >= 50) {
+                        if (difference >= 0) {
+                            index = 2;
+                            if (previousIndex == 0) {
+                                index = 1;
+                            }
+                        } else {
+                            if (previousIndex == 0) {
+                                index = 3;
+                            }
+                        }
+                    } else {
+                        index = 1;
+                        difference -= 50;
+                        if (difference >= 0) {
+                            if (previousIndex == 3) {
+                                index = 0;
+                            }
+                        } else {
+                            if (previousIndex == 3) {
+                                index = 2;
+                            }
+                        }
+                    }
+                } else {
+                    if (yPercent >= 50) {
+                        index = 3;
+                        difference += 50;
+                        if (difference >= 0) {
+                            if (previousIndex == 1) {
+                                index = 0;
+                            }
+                        } else {
+                            if (previousIndex == 1) {
+                                index = 2;
+                            }
+                        }
+                    } else {
+                        index = 0;
+                        if (difference >= 0) {
+                            if (previousIndex == 2) {
+                                index = 1;
+                            }
+                        } else {
+                            if (previousIndex == 2) {
+                                index = 3;
+                            }
+                        }
+                    }
+                }
+
                 gotoLocation = micePointLocations[index];
-                index += 1;
 
             }
-            else
-            {
                 if (miceLocation.X == gotoLocation.X)
                 {
                     x_direction = 0f;
@@ -98,7 +155,7 @@ namespace SnackAttack.Desktop
                 }
                 miceLocation.X += (float)Math.Round((miceSpeed * x_direction) * (float)gameTime.ElapsedGameTime.TotalSeconds);
                 miceLocation.Y += (float)Math.Round((miceSpeed * y_direction) * (float)gameTime.ElapsedGameTime.TotalSeconds);
-            }
+                // Console.WriteLine(miceLocation.X + ":::::::" + miceLocation.Y);
 
             return miceLocation;
         }
